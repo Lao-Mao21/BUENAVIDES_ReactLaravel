3b2cd63268c6b74ddfb6e69133f9e0d47b8535cd
Nas_0o0 <BuenavidesJonas@gmail.com>
2026-03-16 02:24:06 -0700

    Add files via upload
    new additions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,32 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let name: string = "Jonas";
+  let age: number = 20;
+
+  //comment
+  //embed JS expression
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> {/*Fragment*/}
+      <h1 className='font-mono font-bold text-3xl bg-clip-text text-transparent bg-linear-to-b from-blue-600 via-sky-500 to-purple-500'>Student Information</h1>
+      <p className='font-mono text-xl'>Name: {name}</p>
+      <p className='font-mono text-xl'>Age: {age} Years Old</p>
     </>
   )
 }
-
 export default App
