c772226442e9deedca005343ec64cb7546f76c86
Nas_0o0 <BuenavidesJonas@gmail.com>
2026-03-16 02:32:56 -0700

    Add files via upload
    new uploads

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,10 @@ import './App.css'
 function App() {
   let name: string = "Jonas";
   let age: number = 20;
-
+  let score1: number = 92
+  let score2: number = 50
+  let totalScore: number = score1 + score2
+  let averageScore: number = totalScore / 2
   //comment
   //embed JS expression
 
@@ -15,6 +18,10 @@ function App() {
       <h1 className='font-mono font-bold text-3xl bg-clip-text text-transparent bg-linear-to-b from-blue-600 via-sky-500 to-purple-500'>Student Information</h1>
       <p className='font-mono text-xl'>Name: {name}</p>
       <p className='font-mono text-xl'>Age: {age} Years Old</p>
+      <p className='font-mono text-xl'>Score 1: {score2}</p>
+      <p className='font-mono text-xl'>Score 2: {score2}</p>
+      <p className='font-mono text-xl'>Total Score: {totalScore}</p>
+      <p className='font-mono text-xl'>Average Score: {averageScore}</p>
     </>
   )
 }
