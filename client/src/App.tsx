08fdcf8e43609ae8e95e4b0197e6448b71573bcf
Lao-Mao21 <buenavidesjonas@gmail.com>
2026-04-19 13:32:33 +0800

    Final Part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,10 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import { Routes } from "./routes/Routes";
 
-function App() {
-//   let studentName: string = "Jonas C. Buenavides";
-//   let age: number = 67;
-//   let isActive: boolean = false;
-//   console.log("Status:", isActive)
+const App = () => {
 
-//   let status: "Active" | "Inactive";
-//   status = "Active";
-//   let gradeLevel: "Grade 7" | "Grade 8" | "Grade 9";
-//   gradeLevel = "Grade 9";
+  return <RouterProvider router={Routes} />;
 
-//   function getRemarks(score: number): string {
-//     if (score >= 90) {
-//       return("Excellent")
-//     } else if (score >= 75) {
-//       return("Passed")
-//     } else {
-//       return("Failed")
-//     }
-//   }
-//   console.log("Remarks:", getRemarks(99))
+};
 
-//   interface Student {
-//     name: string;
-//     age: number;
-//     gradeLevel: string;
-//     status: "Active" | "Inactive";
-//     score: number;
-//   }
-//   let student1: Student = {
-//     name: "Jonas Buenavides",
-//     age: 41,
-//     gradeLevel: "Grade 8",
-//     status: "Active",
-//     score: 67
-//   };
-//   console.log(student1)
-//   return (
-//     <> {/*Fragment*/}
-//     <h1 className='font-mono font-bold text-3xl bg-clip-text text-transparent bg-linear-to-b from-red-600 via-yellow-400 to-red-600'>Student Profiles</h1>
-//     <br />
-//       <div className='flex items-center flex-col'>
-//         <div className='flex flex-col items-start w-md'>
-//           <p className='font-mono text-xl'>Student Name: {studentName}</p>
-//           <p className='font-mono text-xl'>Age: {age}</p>
-//           <p className='font-mono text-xl'>Grade Level: {gradeLevel}</p>
-//           <p className='font-mono text-xl'>Is Active: {isActive ? "Yes" : "No"}</p>
-//           <p className='font-mono text-xl'>Status: {status}</p>
-//         </div>
-//         <br />
-//         <div className='flex flex-col items-start w-md'>
-//           <p className='font-mono text-xl'>Student Name: {student1.name}</p>
-//           <p className='font-mono text-xl'>Age: {student1.age}</p>
-//           <p className='font-mono text-xl'>Is Active: {student1.gradeLevel}</p>
-//           <p className='font-mono text-xl'>Status: {student1.status}</p>
-//           <p className='font-mono text-xl'>Score: {student1.score}</p>
-//           <p className='font-mono text-xl'>Status: {getRemarks(student1.score)}</p>
-//         </div>
-//       </div>
-//     </>
-//   )
-}
-export default App
+export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' //[!code ++]
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), //[!code ++]
+    tailwindcss(),
   ],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
